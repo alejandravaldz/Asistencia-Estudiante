410ec42e2221d228324415556af27255a6dfa9b2
Alejandra <153337125+alejandravaldz@users.noreply.github.com>
2026-07-31 00:50:17 -0400

    ....cs

--- Form1.cs
+++ Form1.cs
@@ -7,18 +7,49 @@ namespace Asistencia_Estudiante
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void btn_Agregar_Click(object sender, EventArgs e)
         {
-           
+       
+    string Asistencia;
+    if (chbo_Presente.Checked)
+    {
+        Asistencia = "Presente";
+    }
+    else
+    {
+        Asistencia = "Ausente";
+    }
+    lst_Asistencia.Items.Add(txt_Nombre.Text + "  " + Asistencia);
+    txt_Nombre.Clear();
+
+    chbo_Presente.Checked = false;
+    chbox_Ausencia.Checked = false;
+}
+
+
+        private void btn_Resumen_Click(object sender, EventArgs e)
+ {
+     int presentes = 0;
+     int ausentes = 0;
+
+     foreach (var item in lst_Asistencia.Items)
+     {
+         string registro = item.ToString();
+         if (registro.Contains("Presente"))
+             presentes++;
+         else
+             ausentes++;
+     }
+
+     MessageBox.Show($"Resumen de asistencia:\nPresentes: {presentes}\nAusentes: {ausentes}");
+ }
+
+ 
+
+         private void btn_Salir_Click(object sender, EventArgs e)
+ {
+      Close();
+ } 
     }
 }
